fe351aefbd0f01c56fc0f59185794cfe211da9d0
Constantino Flouras <tino@bridgefusion.com>
2020-09-04 16:40:39 +0100

    Adjusted the serial menu to use USART3 and the J18 header. USART3 now receives data based off of interrupts.

---
+++ Inc/ModbusDataModel.h
@@ -0,0 +1,49 @@
+/*
+ * ModbusDataModel.h
+ *
+ *  Created on: Aug 28, 2020
+ *      Author: BFS
+ */
+
+#ifndef MODBUSINTERFACE_H_
+#define MODBUSDATAMODEL_H_
+
+#define HOLDING_REGISTER(addr, str, read, write) \
+	case addr: \
+		pReadFunction = read; \
+		pWriteFunction = write; \
+		break;
+#define FOREACH_HOLDING_REGISTER(HOLDING_REGISTER) \
+	HOLDING_REGISTER(00000,	"Polling",				ModbusDataModel_ReturnResetState_uint16_t,	NULL) \
+	HOLDING_REGISTER(00010,	"Polling",				ModbusDataModel_ReturnResetState_uint16_t,	NULL) \
+	HOLDING_REGISTER(42100, "Parameter Unlock", 	NULL, NULL) \
+	//	To be continued.
+
+
+
+#define INPUT_REGISTER(addr, str, read, write) \
+	case addr: \
+		pReadFunction = read; \
+		pWriteFunction = write; \
+		break;
+#define FOREACH_INPUT_REGISTER(INPUT_REGISTER) \
+	INPUT_REGISTER(31100, "Offline Mode Status", 	NULL, NULL), \
+	INPUT_REGISTER(31101, "Temperature (C)", 		NULL, NULL), \
+	INPUT_REGISTER(31102, "Fault Code", 			NULL, NULL),
+	//	To be continued.
+
+#define COIL(addr, str, read, write) \
+	case addr: \
+		pReadFunction = read; \
+		pWriteFunction = write; \
+		break;
+#define FOREACH_COIL(COIL) \
+	COIL(04100, "Restart", 				NULL, NULL) \
+	COIL(04101, "Factory Reset", 		NULL, NULL) \
+	COIL(04102, "Clear Last Faults", 	NULL, NULL) \
+	//	To be continued.
+
+bool ModbusDataModel_ReadCoil(uint16_t nAddress, bool * bReturn);
+bool ModbusDataModel_ReadHoldingRegister(uint16_t nAddress, uint16_t * nReturn);
+
+#endif /* MODBUSINTERFACE_H_ */
--- Src/stm32l4xx_it.c
+++ Src/stm32l4xx_it.c
@@ -57,12 +57,14 @@
 
 /* External variables --------------------------------------------------------*/
 extern DMA_HandleTypeDef hdma_adc1;
+extern UART_HandleTypeDef huart1;
+extern UART_HandleTypeDef huart3;
 /* USER CODE BEGIN EV */
 
 /* USER CODE END EV */
 
 /******************************************************************************/
-/*           Cortex-M4 Processor Interruption and Exception Handlers          */ 
+/*           Cortex-M4 Processor Interruption and Exception Handlers          */
 /******************************************************************************/
 /**
   * @brief This function handles Non maskable interrupt.
@@ -209,6 +211,34 @@ void DMA1_Channel1_IRQHandler(void)
   /* USER CODE BEGIN DMA1_Channel1_IRQn 1 */
 
   /* USER CODE END DMA1_Channel1_IRQn 1 */
+}
+
+/**
+  * @brief This function handles USART1 global interrupt.
+  */
+void USART1_IRQHandler(void)
+{
+  /* USER CODE BEGIN USART1_IRQn 0 */
+
+  /* USER CODE END USART1_IRQn 0 */
+  HAL_UART_IRQHandler(&huart1);
+  /* USER CODE BEGIN USART1_IRQn 1 */
+
+  /* USER CODE END USART1_IRQn 1 */
+}
+
+/**
+  * @brief This function handles USART3 global interrupt.
+  */
+void USART3_IRQHandler(void)
+{
+  /* USER CODE BEGIN USART3_IRQn 0 */
+
+  /* USER CODE END USART3_IRQn 0 */
+  HAL_UART_IRQHandler(&huart3);
+  /* USER CODE BEGIN USART3_IRQn 1 */
+
+  /* USER CODE END USART3_IRQn 1 */
 }
 
 /* USER CODE BEGIN 1 */
