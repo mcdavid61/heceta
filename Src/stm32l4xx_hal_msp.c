dfb28905b4437946bbfa551b1937721c839306e4
Constantino Flouras <tino@bridgefusion.com>
2020-09-30 23:36:24 -0400

    [HRM-23] A working version of code that correctly wrote/read back a single page over SPI to the EEPROM

--- Src/stm32l4xx_hal_msp.c
+++ Src/stm32l4xx_hal_msp.c
@@ -245,6 +245,9 @@ void HAL_SPI_MspInit(SPI_HandleTypeDef* hspi)
     GPIO_InitStruct.Alternate = GPIO_AF5_SPI1;
     HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
 
+    /* SPI1 interrupt Init */
+    HAL_NVIC_SetPriority(SPI1_IRQn, 0, 0);
+    HAL_NVIC_EnableIRQ(SPI1_IRQn);
   /* USER CODE BEGIN SPI1_MspInit 1 */
 
   /* USER CODE END SPI1_MspInit 1 */
@@ -275,6 +278,8 @@ void HAL_SPI_MspDeInit(SPI_HandleTypeDef* hspi)
     */
     HAL_GPIO_DeInit(GPIOA, EE_SCK_Pin|EE_MISO_Pin|EE_MISOA7_Pin);
 
+    /* SPI1 interrupt DeInit */
+    HAL_NVIC_DisableIRQ(SPI1_IRQn);
   /* USER CODE BEGIN SPI1_MspDeInit 1 */
 
   /* USER CODE END SPI1_MspDeInit 1 */
