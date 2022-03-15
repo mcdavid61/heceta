dd3053b90151ea773243690c383c90af124b44f6
David McMasters <david.mcmasters@msasafety.com>
2022-03-15 14:56:56 -0400

    HRM-HRM-151 Removed 'pause' from post_build_steps.bat for headless build.

--- post_build_steps.bat
+++ post_build_steps.bat
@@ -11,7 +11,7 @@ echo.
 echo Generating the CRC checksum...
 C:\ST\STM32CubeIDE_1.4.0\STM32CubeIDE\plugins\com.st.stm32cube.ide.mcu.externaltools.gnu-tools-for-stm32.7-2018-q2-update.win32_1.4.0.202007081208\tools\arm-none-eabi\bin\objcopy.exe -O ihex "%project_name%.elf"  "%project_name%.hex"
 ..\srecord-1.64-win32\srec_cat.exe "%project_name%.hex" -Intel -fill 0xFF 0x08000000 0x0801FFFC -STM32 0x0801FFFC -o "%project_name%_CRC.hex" -Intel
-pause
+:: pause
 exit
 
 
